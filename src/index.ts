1045f2b264567112b96a9ff4442fb4bfcea7af4d
ideen1 <ibanijamali@gmail.com>
2024-03-30 12:17:42 -0600

    Fix build dist settings

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { SCIM } from "./SCIM.js";
+
+export default SCIM;
+
+export * from "./SCIM.js";
+export * from "./Resource.js";
+export * from "./ListResource.js";
+export * from "./Patch.js";
+export * from "./SCIMError.js";
+// export * from "./Group";
+export * from "./User.js";
